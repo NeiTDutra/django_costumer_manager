b957487c29ae4c0651a73b15e4faa38026770160
nei <nei.thomass@gmail.com>
2020-08-18 18:44:25 -0300

    adicionado campo de busca por nome

--- customer/views.py
+++ customer/views.py
@@ -1,13 +1,25 @@
-from django.views.generic import ListView, CreateView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from customer.models import Customer
 from customer.forms import CustomerForm
-from django.urls import  reverse
+from django.urls import reverse
+from django.db.models import Q
 
 
 class CustomerListView(ListView):
     template_name = 'customer-list.html'
+    paginate_by = 2
     model = Customer
-    queryset = Customer.objects.all()
+
+    def get_queryset(self):
+        name = self.request.GET.get('by_name')
+        if name:
+            object_list = self.model.objects.filter(
+                Q(first_name__icontains=name) | Q(last_name__icontains=name)
+            )
+        else:
+            object_list = self.model.objects.all()
+        return object_list
 
 
 class CustomerCreateView(CreateView):
@@ -16,6 +28,31 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+
+
+class CustomerUpdateView(UpdateView):
+    template_name = 'customer-create.html'
+    form_class = CustomerForm
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Customer, id=id)
+    
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+
+
+class CustomerDeleteView(DeleteView):
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Customer, id=id)
 
     def get_success_url(self):
         return reverse('customer:customer-list')
